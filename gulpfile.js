e3dd312c33c14133a0d8638972aba0c8efca071d
houser <houserqu@qq.com>
2019-04-05 09:59:30 +0800

    feat: 静态文件拷贝，less编译压缩

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,8 @@ var tsProject = ts.createProject("tsconfig.json")
 var browserify = require("browserify")
 var source = require("vinyl-source-stream")
 var tsify = require("tsify")
+var less = require("gulp-less")
+var cssmin = require("gulp-cssmin")
 
 gulp.task("del", function() {
   return del("dist/**")
@@ -18,6 +20,7 @@ gulp.task("del", function() {
 //     .js.pipe(gulp.dest("dist/js"))
 // })
 
+// 编译 ejs 模板
 gulp.task("ejs", function() {
   return gulp
     .src("./src/*.ejs")
@@ -33,29 +36,55 @@ gulp.task("ejs", function() {
     .pipe(gulp.dest("./dist"))
 })
 
+// 拷贝无需编译的静态文件
 gulp.task("copy-public", function() {
-  return gulp.src(["./src/public/**/*"]).pipe(gulp.dest("./dist"))
+  return gulp
+    .src(
+      [
+        "./src/**/*",
+        "!./src/*.ejs",
+        "!./src/style/**",
+        "!./src/template-public/**",
+        "!./src/js/**"
+      ],
+      {
+        nodir: true
+      }
+    )
+    .pipe(gulp.dest("./dist"))
 })
 
+// 拷贝 js 库
 gulp.task("copy-js-vendor", function() {
   return gulp.src(["./src/js/vendor/**/*"]).pipe(gulp.dest("./dist/js/vendor"))
 })
 
+// 拷贝 js 库
+gulp.task("ts", function() {
+  return browserify({
+    basedir: ".",
+    debug: true,
+    entries: ["src/js/index.ts"],
+    cache: {},
+    packageCache: {}
+  })
+    .plugin(tsify)
+    .bundle()
+    .pipe(source("bundle.js"))
+    .pipe(gulp.dest("dist/js"))
+})
+
+gulp.task("less", function() {
+  return gulp
+    .src("./src/style/**/*.less")
+    .pipe(less())
+    .pipe(cssmin())
+    .pipe(gulp.dest("./dist/style"))
+})
+
 gulp.task(
   "default",
-  gulp.series("del", "ejs", "copy-public", "copy-js-vendor", function() {
-    return browserify({
-      basedir: ".",
-      debug: true,
-      entries: ["src/js/index.ts"],
-      cache: {},
-      packageCache: {}
-    })
-      .plugin(tsify)
-      .bundle()
-      .pipe(source("bundle.js"))
-      .pipe(gulp.dest("dist/js"))
-  })
+  gulp.series("del", "ejs", "ts", "less", "copy-public", "copy-js-vendor")
 )
 
 // gulp.task("default", gulpSequence("build"))
--- src/js/vendor/jquery.js
+++ src/js/vendor/jquery.js
@@ -1,1 +1,3 @@
 console.log("jquery")
+
+window.Jquery = {}
